dc9e9e1544a44204615fe094fce3c1031cbba485
Jossy <contacto.thayki@gmail.com>
2026-02-08 23:24:07 -0500

    Navbar: THAYKI -> THAYQUI

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -11,7 +11,7 @@ export default function Navigation() {
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex justify-between items-center h-16">
 					<Link href="/" className="flex items-center">
-						<span className="text-xl font-bold text-[#563256]">THAYKI</span>
+						<span className="text-xl font-bold text-[#563256]">THAYQUI</span>
 						<span className="ml-2 text-sm text-[#645c9f] hidden sm:inline">
 							THAYLUS CONSULTING
 						</span>
