7f82cd208dca30141ca3a32c948ff80c393659c1
contactothayki <contacto.thayki@gmail.com>
2026-05-01 17:38:38 -0500

    Update phone number in WhatsAppButton component

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 export default function WhatsAppButton() {
-	const phone = '51980527151';
+	const phone = '51924738467';
 	const text = encodeURIComponent('Hola, quiero más información sobre la efectivización de tarjeta.');
 
 	return (
